fa1c14d95e44a0f36bc33d3fe7ed48504cd80b61
Felix Locquet <felix.locquet@outlook.fr>
2026-04-25 17:59:29 -0200

    Fichier avec la variable data. plus utilisé, pour archivage

--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,121 @@
 let data = {
     "listeComptes" : [
-        { "symbole" : "J.C", "libelle" : "Joint courant"},
-        { "symbole" : "J.CSL", "libelle" : "Joint CSL"},
-        { "symbole" : "S.C", "libelle" : "Sophie courant"} 
+        { "id" : 1, "symbole" : "J.C", "libelle" : "Joint courant"},
+        { "id" : 2, "symbole" : "J.CSL", "libelle" : "Joint CSL"},
+        { "id" : 3, "symbole" : "S.C", "libelle" : "Sophie courant"} 
     ],
     "listeCharge" : [
-        { "symbole" : "J", "libelle" : "Joint"},
-        { "symbole" : "S", "libelle" : "Sophie"},
-        { "symbole" : "F", "libelle" : "Félix"} 
+        { "id" : 1, "symbole" : "J", "libelle" : "Joint"},
+        { "id" : 2, "symbole" : "S", "libelle" : "Sophie"},
+        { "id" : 3, "symbole" : "F", "libelle" : "Félix"} 
+    ],
+    "categories" : [
+        {"id" : 1, "nom" : "Assurances"},
+        {"id" : 2, "nom" : "Audiovisuel"},
+        {"id" : 3, "nom" : "Cadeaux"},
+        {"id" : 4, "nom" : "EDF"},
+        {"id" : 5, "nom" : "Emprunts"},
+        {"id" : 6, "nom" : "Epargne"},
+        {"id" : 7, "nom" : "Fioul"},
+        {"id" : 8, "nom" : "Frais bancaires"},
+        {"id" : 9, "nom" : "Impôts"},
+        {"id" : 10, "nom" : "Maison"},
+        {"id" : 11, "nom" : "Mutuelle"},
+        {"id" : 12, "nom" : "Pension"},
+        {"id" : 13, "nom" : "Personnel"},
+        {"id" : 14, "nom" : "Télécom"},
+        {"id" : 15, "nom" : "Quotidien"},
+        {"id" : 16, "nom" : "Véhicules"},
+        {"id" : 17, "nom" : "Voyages"}
+    ],
+    "entrees" : [
+        {
+            "id" : 1,
+            "liste" : [
+                {"id" : 1, "libelle" : "Chevrolet", "montant" : 41.81, "idComptePayeDepuis" : 1, "idCharge" : 1}
+            ]
+        },
+        {
+            "id" : 2,
+            "liste" : [
+                {"id" : 1, "libelle" : "Netflix", "montant" : 14.99, "idComptePayeDepuis" : 1, "idCharge" : 1},
+                {"id" : 2, "libelle" : "Prime", "montant" : 5.83, "idComptePayeDepuis" : 2, "idCharge" : 1}
+            ]
+        },
+        {
+            "id" : 3,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 4,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 5,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 6,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 7,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 8,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 9,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 10,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 11,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 12,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 13,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 14,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 15,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 16,
+            "liste" : [
+            ]
+        },
+        {
+            "id" : 17,
+            "liste" : [
+            ]
+        }
     ]
 }
